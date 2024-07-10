6bd2d0c282461cde1ff2b0883ce60a7ea1fe3ef1
David <davidmorgadegil@gmail.com>
2024-07-10 23:23:55 +0200

    improved config added dashboard pending to change

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -17,7 +17,46 @@ require('lazy').setup({
   { 'tpope/vim-surround' },
   --React syntax highlight
   { 'maxmellon/vim-jsx-pretty' },
-
+  -- Great start screen
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup({
+        theme = 'hyper',
+        config = {
+          week_header = {
+            enable = true,
+            concat = 'Morgade'
+          },
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              desc = ' Apps',
+              group = 'DiagnosticHint',
+              action = 'Telescope app',
+              key = 'a',
+            },
+            {
+              desc = ' dotfiles',
+              group = 'Number',
+              action = 'Telescope dotfiles',
+              key = 'd',
+            },
+          },
+        },
+      })
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+  },
   -- Color scheme
   { "catppuccin/nvim",               as = "catppuccin" },
 
