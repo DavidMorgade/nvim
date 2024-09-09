e08eb1df2048c72e2f558511da6cb66d0e96a1d0
David <davidmorgadegil@gmail.com>
2024-09-09 22:38:46 +0200

    changed toggle term to a more beaurtiful terminal, now is a float at the bottom center

--- lua/morgade/plugins/toggleterm.lua
+++ lua/morgade/plugins/toggleterm.lua
@@ -3,5 +3,19 @@ return {
   version = "*",
   opts = {
     open_mapping = [[<c-j>]],
+    title_pos = 'center',
+    direction = 'float',
+    float_opts = {
+      width = function(_term)
+        local width = math.ceil(vim.o.columns / 2) + 20
+        _term.float_opts.col = math.ceil((vim.o.columns - width) / 2)
+        return width
+      end,
+      height = function(_term)
+        local height = math.ceil(vim.o.lines / 2) - 10
+        _term.float_opts.row = vim.o.lines - height
+        return height
+      end,
+    }
   }
 }
