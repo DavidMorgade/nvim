de21d06f363fd401ff4761462e91fb659f7764de
David <davidmorgadegil@gmail.com>
2024-09-09 22:22:36 +0200

    integrated telescope with chat copilot

--- lua/morgade/core/functions.lua
+++ lua/morgade/core/functions.lua
@@ -1,17 +1,38 @@
 local telescope = require('morgade.plugins.telescope')
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local copilot = require('CopilotChat')
 
 -- Define the path to the Copilot conversations directory
 local copilot_conversations_path = vim.fn.expand("/home/morgade/.local/share/nvim/copilotchat_history")
+
+-- Custom action to open the selected file with Copilot Chat
+local function open_with_copilot(prompt_bufnr)
+  local selected_entry = action_state.get_selected_entry()
+  local filepath = selected_entry.filename
+  -- filename without .json extension
+  local filename = string.sub(filepath, 1, -6)
+  print("Selected file path: " .. (filename or "nil")) -- Debug print
+  actions.close(prompt_bufnr)
+  -- execute this lua command :lua CopilotChatLoad "path/to/file"
+  vim.cmd("CopilotChatLoad " .. filename)
+end
 
 -- Create a custom Telescope picker for Copilot conversations
 function search_copilot_conversations()
   builtin.find_files({
     prompt_title = "Search Copilot Conversations",
     cwd = copilot_conversations_path,
-    hidden = true
+    hidden = true,
+    attach_mappings = function(_, map)
+      map('i', '<CR>', open_with_copilot)
+      map('n', '<CR>', open_with_copilot)
+      return true
+    end
   })
 end
 
 -- Map the custom picker to a keybinding
-vim.api.nvim_set_keymap('n', '<leader>sc', ':lua search_copilot_conversations()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sc', ':lua search_copilot_conversations()<CR>',
+  { noremap = true, silent = true, desc = "[S]earch Copilot Conversations" })
